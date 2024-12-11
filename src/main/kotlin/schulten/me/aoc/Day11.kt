bfd126d86aa0cc3b976d2cc03d4ad90a47a22eda
Dries Schulten <dries@schulten.me>
2024-12-11 19:42:04 +0100

    Add day 11 part 2.

--- src/main/kotlin/schulten/me/aoc/Day11.kt
+++ src/main/kotlin/schulten/me/aoc/Day11.kt
@@ -3,24 +3,33 @@ package schulten.me.aoc
 fun main() {
   val input = text("/input/Day11.txt").numbers().map { it.toLong() }
 
-  fun run(times: Int): Int {
-    val result = (0..<times).fold(input) { acc, _ ->
-      acc.flatMap { value ->
-        if (value == 0L) {
-          listOf(1)
-        } else if (value.toString().length % 2 == 0) {
-          val text = value.toString()
-          val length = text.length / 2
-          listOf(text.take(length).toLong(), text.drop(length).toLong())
-        } else {
-          listOf(value * 2024)
-        }
+  fun run(times: Int): Long {
+    fun calc(value: Long): List<Long> = when {
+      value == 0L -> listOf(1L)
+      value.toString().length % 2 == 0 -> {
+        val text = value.toString()
+        val length = text.length / 2
+        listOf(text.take(length).toLong(), text.drop(length).toLong())
       }
+      else -> listOf(value * 2024)
     }
 
-    return result.size
+    val grouped = input.groupBy { it }.mapValues { it.value.size.toLong() }
+
+    return (0..<times).fold(grouped) { acc, _ ->
+      val temp = acc.map { (value, count) ->
+        calc(value).groupBy { it }.mapValues { it.value.size }.map { entry ->
+          entry.key to entry.value * count
+        }.toMap()
+      }
+
+      temp.flatMap { it.entries }.groupBy({ it.key }, { it.value }).mapValues { it.value.sum() }
+    }.values.sum()
   }
 
+  // Part 1
   println(run(25))
+
+  // Part 2
   println(run(75))
 }
